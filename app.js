cbae6d7f5bad880e8e1f85be8d4b0f51c8ea4e2a
deniskidagi <deniskidagi@gmail.com>
2021-11-29 03:43:27 +0000

    complete OAuth2.0 sign in with google

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const ejs = require("ejs")
 const session = require("express-session")
 const passport = require("passport")
 const passportLocalMongoose = require("passport-local-mongoose")
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const FacebookStrategy = require('passport-facebook').Strategy
+const findOrCreate = require('mongoose-findorcreate')
+
 
 
 const app = express()
@@ -20,6 +24,7 @@ app.use(session({
     saveUninitialized: false
 }))
 
+
 app.use(passport.initialize())
 app.use(passport.session())
 
@@ -27,19 +32,55 @@ mongoose.connect("mongodb://localhost:27017/userDB")
 
 const userSchema = new mongoose.Schema ({
     email: String,
-    password: String
+    password: String,
+    googleId: String,
+    facebookId: String,
+    secret: String
 })
 
 userSchema.plugin(passportLocalMongoose)
-
+userSchema.plugin(findOrCreate)
 const User = mongoose.model("User", userSchema)
 
 // use static authenticate method of model in LocalStrategy
 passport.use(User.createStrategy());
 
 // use static serialize and deserialize of model for passport session support
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+//facebook auth
+passport.use(new FacebookStrategy({
+    clientID: process.env.FCLIENT_ID,
+    clientSecret: process.env.FCLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 
 app.get("/", function(req, res){
@@ -51,17 +92,70 @@ app.get("/login", function(req, res){
 app.get("/register", function(req, res){
     res.render('register')
 })
+
 app.get("/secrets", function(req, res){
+    User.find({"secrets": {$ne: null}}, function(err, foundUser){
+      if(err){
+        console.log(err)
+      }else{
+        if(foundUser){
+          res.render("secrets", {userWithSecrets: foundUser})
+        }
+      }
+    })
+})
+app.get("/submit", function(req, res){
     if(req.isAuthenticated()){
-        res.render("secrets")
+        res.render("submit")
     }else {
         res.redirect("/login")
     }
 })
+
+app.post("/submit", function(req, res){
+  const submittedSecret = req.body.secret
+  User.findById(req.user.id, function(err, foundUser){
+    if(err){
+      console.log(err)
+    }else{
+      if(foundUser){
+        foundUser.secret = submittedSecret
+        foundUser.save(function(){
+          res.redirect('/secrets')
+        })
+      }
+    }
+  })
+})
+
 app.get("/logout", function(req, res){
     req.logout()
     res.redirect("/")
 })
+
+app.get("/auth/google",
+  passport.authenticate("google", { scope: ["profile"] }));
+
+
+app.get("/auth/google/secrets", 
+passport.authenticate("google", { failureRedirect: "/login" }),
+function(req, res) {
+// Successful authentication, redirect home.
+res.redirect("/");
+});
+
+
+//facebook authenticate
+app.get('/auth/facebook',
+  passport.authenticate('facebook', { scope: ["profile"] }));
+
+app.get('/auth/facebook/secrets',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+
 app.post("/register", function(req, res){
     User.register({username: req.body.username}, req.body.password, function(err, user){
         if(err){
